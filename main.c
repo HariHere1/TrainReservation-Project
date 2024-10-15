03c7845ab6defbaaa372b6b8b85e61eb74928f69
Harigovind P <112310327+HariHere1@users.noreply.github.com>
2024-10-15 20:55:45 +0530

    Update Train_resrv.c

--- main.c
+++ main.c
@@ -2,321 +2,94 @@
 #include <string.h>
 #include <ctype.h>
 
-int class_op(){
-    printf("Choose your ticket class");
-    printf("Class 1 : $200 | Class 2 : $100 | Class 3 : $50");
+// Function to select the class of ticket
+int class_op() {
+    printf("\nChoose your ticket class\n");
+    printf("Class 1: $200 | Class 2: $100 | Class 3: $50\n");
     int price_op;
-    scanf("%d",&price_op);
-    
+    scanf("%d", &price_op);
+    return price_op;
 }
 
-int payment_pro(){
-    printf("Requesting payment for ");
-}
-
-int main(){
-
-    printf("Enter your login id : ");
-    int login_id;
-    scanf("%d",&login_id);
-    printf("\nTRAIN NO -> 1 | New York to Boston | Departure: 08:00 AM | Arrival: 11:30 AM | Date: 12-Oct-2024");
-    printf("\n\nTRAIN NO -> 2 | San Francisco to Los Angeles | Departure: 09:15 AM | Arrival: 02:00 PM | Date: 12-Oct-2024");
-    printf("\n\nTRAIN NO -> 3 | Miami to Tampa | Departure: 07:00 AM | Arrival: 10:30 AM | Date: 12-Oct-2024");
-    
-    printf("\n\nEnter your train number : ");
-    int trainNum; /*Getting train num*/
-    scanf("%d",&trainNum);
-
-    int pay_op;
-    char card_num[5];
+// Function to verify card number
+int verify_card(char card_num[]) {
     int valid = 0;
-    int i;
+    if (strlen(card_num) == 5) {
+        for (int i = 0; i < 5; i++) {
+            if (isdigit(card_num[i])) {
+                valid = 1;
+            } else {
+                valid = 0;
+                break;
+            }
+        }
+        if (valid == 0) {
+            printf("Enter a valid card number\n");
+        } else {
+            printf("Processing payment...\n");
+        }
+    } else {
+        printf("Enter a valid card number\n");
+        valid = 0;
+    }
+    return valid;
+}
 
-    switch(trainNum) /*First switch*/
-    {
+// Function for payment process
+void payment_pro(int pay_op) {
+    char card_num[6];
+    int valid = 0;
+
+    switch (pay_op) {
     case 1:
-        printf("\nNew York to Boston | Departure: 08:00 AM | Arrival: 11:30 AM | \nSeats available : 12");
-        printf("\n\nPayment Methods -- 1 -> Credit Card, 2 -> Debit Card, 3 -> Net Banking");
-        printf("\n\nEnter your payment option : ");
-        scanf("%d", &pay_op);
-        switch (pay_op) /*Nested sswitch 1*/
-        {
-        case 1 :
-           do{ printf("\nEnter your 5 digit credit card number : ");
-            scanf("%s", &card_num);
-            if (strlen(card_num)==5){
-                for ( i = 0; i < 5; i++)
-            {
-                if(isdigit(card_num[i])){
-                    valid = 1;
-                }
-
-                else {
-                    valid = 0;
-                }
-            }
-            if(valid == 0) {            
-                 printf("Enter a valid card number");
-            }
-             else {
-                printf("\n Working on it....");
-             }
-            }
-            else {
-                printf("Enter a valid card number");
-            }}while(valid==0);
-            break;
-        
-        case 2 :
-            printf("Enter your 5  debit card number : ");
-            scanf("%s", &card_num);
-             do{ printf("\nEnter your 5 digit credit card number : ");
-            scanf("%s", &card_num);
-            if (strlen(card_num)==5){
-                for ( i = 0; i < 5; i++)
-            {
-                if(isdigit(card_num[i])){
-                    valid = 1;
-                }
-
-                else {
-                    valid = 0;
-                }
-            }
-            if(valid == 0) {            
-                 printf("Enter a valid card number");
-            }
-             else {
-                printf("\n Working on it....");
-             }
-            }
-            else {
-                printf("Enter a valid card number");
-            }}while(valid==0);
-            break;
-            
-        case 3 :
-            printf("Enter your UPI id : ");
-            scanf("%s", &card_num);
-            do{ 
-                printf("\nEnter your 5 digit credit card number : ");
-                scanf("%s", &card_num);
-            if(strlen(card_num)==5){
-                for ( i = 0; i < 5; i++)
-            {
-                if(isdigit(card_num[i])){
-                    valid = 1;
-                }
-
-                else {
-                    valid = 0;
-                }
-            }
-            if(valid == 0) {            
-                 printf("Enter a valid card number");
-            }
-             else {
-                printf("\n Working on it....");
-             }
-            }
-            else {
-                printf("Enter a valid card number");
-            }}while(valid==0);
-            break;
-
-        default:
-            printf("Enter a valid payment option : ");
-            break;
-        }
-        break;
-
     case 2:
-        printf("\nSan Francisco to Los Angeles | Departure: 09:15 AM | Arrival: 02:00 PM | Date: 12-Oct-2024\nSeats available : 12 ");
-        printf("\n\nPayment Methods - 1 -> Credit Card, 2 -> Debit Card, 3 -> Net Banking");
-        printf("\n\nEnter your payment option : ");
-        scanf("%d", &pay_op);
-          switch (pay_op) /*Nested sswitch 1*/
-        {
-        case 1 :
-             do{ printf("\nEnter your 5 digit credit card number : ");
-            scanf("%s", &card_num);
-            if (strlen(card_num)==5){
-                for ( i = 0; i < 5; i++)
-            {
-                if(isdigit(card_num[i])){
-                    valid = 1;
-                }
-
-                else {
-                    valid = 0;
-                }
-            }
-            if(valid == 0) {            
-                 printf("Enter a valid card number");
-            }
-             else {
-                printf("\n Working on it....");
-             }
-            }
-            else {
-                printf("Enter a valid card number");
-            }}while(valid==0);
-            break;
-        
-        case 2 :
-             do{ printf("\nEnter your 5 digit credit card number : ");
-            scanf("%s", &card_num);
-            if (strlen(card_num)==5){
-                for ( i = 0; i < 5; i++)
-            {
-                if(isdigit(card_num[i])){
-                    valid = 1;
-                }
-
-                else {
-                    valid = 0;
-                }
-            }
-            if(valid == 0) {            
-                 printf("Enter a valid card number");
-            }
-             else {
-                printf("\n Working on it....");
-             }
-            }
-            else {
-                printf("Enter a valid card number");
-            }}while(valid==0);
-            
-        case 3 :
-             do{ printf("\nEnter your 5 digit credit card number : ");
-            scanf("%s", &card_num);
-            if (strlen(card_num)==5){
-                for ( i = 0; i < 5; i++)
-            {
-                if(isdigit(card_num[i])){
-                    valid = 1;
-                }
-
-                else {
-                    valid = 0;
-                }
-            }
-            if(valid == 0) {            
-                 printf("Enter a valid card number");
-            }
-             else {
-                printf("\n Working on it....");
-             }
-            }
-            else {
-                printf("Enter a valid card number");
-            }}while(valid==0);
-
-        default:
-            printf("Enter a valid payment option : ");
-            break;
-        }
-        break;
-
     case 3:
-        printf("\nMiami to Tampa | Departure: 07:00 AM | Arrival: 10:30 AM | Date: 12-Oct-2024 \nSeats available : 12");
-        printf("\n\nPayment Methods - 1 -> Credit Card, 2 -> Debit Card, 3 -> Net Banking");
-        printf("\n\nEnter your payment option : ");
-        scanf("%d", &pay_op);
-        switch (pay_op) /*Nested sswitch 1*/
-        {
-        case 1 :
-             do{ printf("\nEnter your 5 digit credit card number : ");
-            scanf("%s", &card_num);
-            if (strlen(card_num)==5){
-                for ( i = 0; i < 5; i++)
-            {
-                if(isdigit(card_num[i])){
-                    valid = 1;
-                }
-
-                else {
-                    valid = 0;
-                }
-            }
-            if(valid == 0) {            
-                 printf("Enter a valid card number");
-            }
-             else {
-                printf("\n Working on it....");
-             }
-            }
-            else {
-                printf("Enter a valid card number");
-            }}while(valid==0);
-            break;
-        
-        case 2 :
-             do{ printf("\nEnter your 5 digit credit card number : ");
-            scanf("%s", &card_num);
-            if (strlen(card_num)==5){
-                for ( i = 0; i < 5; i++)
-            {
-                if(isdigit(card_num[i])){
-                    valid = 1;
-                }
-
-                else {
-                    valid = 0;
-                }
-            }
-            if(valid == 0) {            
-                 printf("Enter a valid card number");
-            }
-             else {
-                printf("\n Working on it....");
-             }
-            }
-            else {
-                printf("Enter a valid card number");
-            }}while(valid==0);
-            break;
-            
-        case 3 :
-             do{ printf("\nEnter your 5 digit credit card number : ");
-            scanf("%s", &card_num);
-            if (strlen(card_num)==5){
-                for ( i = 0; i < 5; i++)
-            {
-                if(isdigit(card_num[i])){
-                    valid = 1;
-                }
-
-                else {
-                    valid = 0;
-                }
-            }
-            if(valid == 0) {            
-                 printf("Enter a valid card number");
-            }
-             else {
-                printf("\n Working on it....");
-             }
-            }
-            else {
-                printf("Enter a valid card number");
-            }}while(valid==0);
-            break;
-
-        default:
-            printf("Enter a valid payment option : ");
-            break;
-        }
-        break;
-
-    case 4 :
-        printf("Exiting");
+        do {
+            printf("\nEnter your 5 digit card number: ");
+            scanf("%s", card_num);
+            valid = verify_card(card_num);
+        } while (valid == 0);
         break;
     default:
-        printf("Invalid Train Number");
+        printf("Enter a valid payment option\n");
         break;
     }
-return 0;
+}
 
+// Main function
+int main() {
+    int login_id, trainNum, pay_op;
+
+    printf("Enter your login id: ");
+    scanf("%d", &login_id);
+
+    printf("\nTRAIN NO -> 1 | New York to Boston | Departure: 08:00 AM | Arrival: 11:30 AM | Date: 12-Oct-2024");
+    printf("\nTRAIN NO -> 2 | San Francisco to Los Angeles | Departure: 09:15 AM | Arrival: 02:00 PM | Date: 12-Oct-2024");
+    printf("\nTRAIN NO -> 3 | Miami to Tampa | Departure: 07:00 AM | Arrival: 10:30 AM | Date: 12-Oct-2024");
+
+    printf("\n\nEnter your train number: ");
+    scanf("%d", &trainNum);
+
+    switch (trainNum) {
+    case 1:
+        printf("\nNew York to Boston | Departure: 08:00 AM | Arrival: 11:30 AM | \nSeats available: 12\n");
+        break;
+    case 2:
+        printf("\nSan Francisco to Los Angeles | Departure: 09:15 AM | Arrival: 02:00 PM | \nSeats available: 12\n");
+        break;
+    case 3:
+        printf("\nMiami to Tampa | Departure: 07:00 AM | Arrival: 10:30 AM | \nSeats available: 12\n");
+        break;
+    default:
+        printf("Invalid Train Number\n");
+        return 0;
+    }
+
+    printf("\nPayment Methods -- 1 -> Credit Card, 2 -> Debit Card, 3 -> Net Banking");
+    printf("\n\nEnter your payment option: ");
+    scanf("%d", &pay_op);
+
+    payment_pro(pay_op);
+
+    return 0;
 }
